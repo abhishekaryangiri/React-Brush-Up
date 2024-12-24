ed9bc47fac5affb37abda2bb7c02dcb3b47d4353
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-24 21:28:02 +0530

    ok

--- day-2-prop-passingarrays/src/Product.jsx
+++ day-2-prop-passingarrays/src/Product.jsx
@@ -1,11 +1,13 @@
 import './Product.css';
 
-function Product({title, price, description}){
+function Product({title, price, description, feature, feature2}){
     return(
         <div className='Product'>
             <h2>{title}</h2>
             <p>price: {price}</p>
             <p>{description}</p>
+            <p>{feature}</p>
+            <p>{feature2.b}</p>
         </div>
     )
 }
--- day-2-prop-passingarrays/src/ProductTab.jsx
+++ day-2-prop-passingarrays/src/ProductTab.jsx
@@ -1,12 +1,30 @@
 import Product from './Product.jsx';
 
 function ProductTab(){
-
+let options = ["fastest selling", "india's best ", "lowest price"];
+let options2 = {a:"available on emi", b:"24hr delivery", c:"visit your bearest dealer" };
         return(
             <>
-                <Product title="I-phone" price={19090} description="Hurry up limited offer"/>
-                <Product title="Laptop" price={29090} description="Fastest selling: sale end today midnight" />
-                <Product title="Nexon" price={1139090} description=" Safest Car of the India: 5* Rating  "/>
+                <Product 
+                title="I-phone" 
+                price={19090} 
+                description="Hurry up limited offer" 
+                feature={options}  
+                feature2={options2}/>
+                
+                <Product 
+                title="Laptop" 
+                price={29090} 
+                description="Fastest selling: sale end today midnight" 
+                feature={options} 
+                feature2={options2}/>
+
+                <Product 
+                title="Nexon" 
+                price={1139090} 
+                description=" Safest Car of the India: 5* Rating" 
+                feature={options}
+                feature2={options2}/>
             </>
         )
 }
